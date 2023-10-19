661ac1e84a4177ac791710a59424a5431e940d8f
educc <educcarnn@gmail.com>
2023-10-19 16:47:47 -0300

    feat: implement validation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main(): 
-    print("Main executado")
+from petshop_handler import manage_stock
 
-if __name__ == "__main__":
+
+def main():
+    print("Main executado")
+    print(manage_stock.add_new_product(name="Ração", price=85.5, avaliable_units=5))
+   
+if __name__ == "__main__": 
     main()
+ 
--- petshop_handler/manage_stock.py
+++ petshop_handler/manage_stock.py
@@ -1,4 +1,20 @@
 from stock import products
 
-def add_new_product(**kwargs: dict): 
-    ...
+def add_new_product(**kwargs: dict):
+    try:
+        received_product = {
+            "name": kwargs["name"],
+            "price": kwargs["price"],
+            "avaliable_units": kwargs["avaliable_units"]
+        }
+    except KeyError:
+        return "Estão faltando chaves obrigatórias no dicionário."
+    
+    if not products:  
+        products.append(received_product)
+        return received_product
+    
+    for current_product in products:
+        if current_product["name"] == received_product["name"]: 
+            current_product["avaliable_units"] += received_product["avaliable_units"]
+            return f"Produto com nome {current_product['name']} foi reabastecido"
